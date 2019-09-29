c8d55b12990668af61cc2c96644e13b5eb7e389b
Emam <emamAhmed36@gmail.com>
2019-09-29 11:49:18 +0200

    add splash screen

---
+++ app/src/main/java/com/example/todolist/splash_screen/SplashActivity.java
@@ -0,0 +1,34 @@
+package com.example.todolist.splash_screen;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.airbnb.lottie.LottieAnimationView;
+import com.example.todolist.MainActivity;
+import com.example.todolist.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    LottieAnimationView lottieAnimationView;
+
+    private final int SPLASH_DISPLAY_LENGTH = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        try {
+            this.getSupportActionBar().hide();
+        } catch (NullPointerException e) {
+        }
+        lottieAnimationView = findViewById(R.id.animation_view);
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
